8b0a91bfcead8a56ab176b608003461615f9dbbb
DatPT0541 <phung.tien.dat@sun-asterisk.com>
2024-08-20 16:11:56 +0700

    send_mail_booking_status

--- app/controllers/admin/booking_fields_controller.rb
+++ app/controllers/admin/booking_fields_controller.rb
@@ -8,9 +8,11 @@ class Admin::BookingFieldsController < Admin::BaseController
     @booking_field = BookingField.find_by id: params[:id]
     respond_to do |format|
       success_message = t ".messages.success_change_status"
-      error_message = t ".messages.failed_change_status"
+      error_message = t ".messages.error_change_status"
 
       if @booking_field.update(booking_field_params)
+        BookingFieldMailer.status_change_notification(@booking_field)
+                          .deliver_now
         flash[:success] = success_message
       else
         flash[:danger] = error_message
