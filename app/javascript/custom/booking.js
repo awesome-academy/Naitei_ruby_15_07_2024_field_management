094d7f1d51cd36f322d36a0798876489f5493ec2
DatPT0541 <phung.tien.dat@sun-asterisk.com>
2024-08-26 10:08:01 +0700

    clean_data_fix

--- app/javascript/custom/booking.js
+++ app/javascript/custom/booking.js
@@ -1,79 +1,79 @@
-document.addEventListener("turbo:load", function() {
-  const blocksField = document.getElementById("blocks");
-  const startTimeField = document.getElementById("start_time");
-  const endTimeField = document.getElementById("end_time");
-  const totalPriceField = document.getElementById("total_price");
+document.addEventListener('turbo:load', function() {
+  const blocksField = document.getElementById('blocks');
+  const startTimeField = document.getElementById('start_time');
+  const endTimeField = document.getElementById('end_time');
+  const totalPriceField = document.getElementById('total_price');
   const voucherCheckboxes = document.querySelectorAll("input[name='voucher_ids[]']");
-  const priceAlert = document.getElementById("price-alert"); 
-  const generalAlert = document.getElementById("general-booking-alert")
-  const pricePerBlock = document.getElementById("price-per-block").textContent; 
-  const blockTime = document.getElementById("block-time").textContent; 
+  const priceAlert = document.getElementById('price-alert');
+  const generalAlert = document.getElementById('general-booking-alert')
+  const pricePerBlock = document.getElementById('price-per-block').textContent;
+  const blockTime = document.getElementById('block-time').textContent;
 
   function calculateEndTimeAndTotalPrice() {
     const blocks = parseInt(blocksField.value) || 0;
     const startTime = startTimeField.value;
     if (startTime) {
-      const startTimeParts = startTime.split(":");
+      const startTimeParts = startTime.split(':');
       let endHour = parseInt(startTimeParts[0]) + blocks * blockTime;
       let endMinutes = startTimeParts[1];
       if (endHour >= 24) {
         endHour -= 24;
       }
-      endTimeField.value = `${String(endHour).padStart(2, "0")}:${endMinutes}`;
+      endTimeField.value = `${String(endHour).padStart(2, '0')}:${endMinutes}`;
     }
     const selectedVouchers = Array.from(voucherCheckboxes)
       .filter(checkbox => checkbox.checked)
-      .map(checkbox => parseFloat(checkbox.getAttribute("data-price")) || 0);
-    
+      .map(checkbox => parseFloat(checkbox.getAttribute('data-price')) || 0);
+
     const totalPrice = (blocks * pricePerBlock) + selectedVouchers.reduce((sum, price) => sum - price, 0);
-    totalPriceField.value = totalPrice.toFixed(2); 
+    totalPriceField.value = totalPrice.toFixed(2);
 
     if (totalPrice < 0) {
-      priceAlert.style.display = "block";
+      priceAlert.style.display = 'block';
       totalPriceField.value = 0
     } else {
-      priceAlert.style.display = "none";
+      priceAlert.style.display = 'none';
     }
   }
 
-  blocksField.addEventListener("input", calculateEndTimeAndTotalPrice);
-  startTimeField.addEventListener("input", calculateEndTimeAndTotalPrice);
+  blocksField.addEventListener('input', calculateEndTimeAndTotalPrice);
+  startTimeField.addEventListener('input', calculateEndTimeAndTotalPrice);
   voucherCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener("change", calculateEndTimeAndTotalPrice);
+    checkbox.addEventListener('change', calculateEndTimeAndTotalPrice);
   });
 
-  const bookingButton = document.getElementById("submit_button_booking");
-  
-  bookingButton.addEventListener("click", function(event) {
+  const bookingButton = document.getElementById('submit_button_booking');
+
+  bookingButton.addEventListener('click', function(event) {
     const startTimeValue = startTimeField.value.trim();
     const totalPrice = totalPriceField.value.trim() ;
-    const dateValue = document.getElementById("user-date-booking").value.trim();
-    
+    const dateValue = document.getElementById('user-date-booking').value.trim();
+
     if (!dateValue || !blockTime || !startTimeValue || totalPrice < 0) {
       event.preventDefault();
-      generalAlert.style.display = "block";
+      generalAlert.style.display = 'block';
     } else {
-      generalAlert.style.display = "block";
+      generalAlert.style.display = 'block';
     }
   });
 
-  var modal = document.getElementById("myModal");
+  var modal = document.getElementById('myModal');
 
-  var btn = document.getElementById("showModal");
+  var btn = document.getElementById('showModal');
 
-  var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName('close')[0];
 
   btn.onclick = function() {
-      modal.style.display = "block";
+      modal.style.display = 'block';
   }
 
   span.onclick = function() {
-      modal.style.display = "none";
+      modal.style.display = 'none';
   }
 
   window.onclick = function(event) {
       if (event.target == modal) {
-          modal.style.display = "none";
+          modal.style.display = 'none';
       }
   }
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_22_064138) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_26_031313) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_22_064138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "paymentStatus", default: 1
+    t.text "cancellation_reason"
     t.index ["field_id"], name: "index_booking_fields_on_field_id"
     t.index ["user_id"], name: "index_booking_fields_on_user_id"
   end
