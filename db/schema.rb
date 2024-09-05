98b14bf633ae42c26bdda18dd22a439ee835e91f
DatPT0541 <phung.tien.dat@sun-asterisk.com>
2024-09-05 15:17:17 +0700

    rspec for field model

--- db/schema.rb
+++ db/schema.rb
@@ -152,6 +152,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_30_023844) do
     t.datetime "updated_at", null: false
     t.string "phone"
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -161,6 +164,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_30_023844) do
     t.datetime "locked_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
--- spec/factories/fields.rb
+++ spec/factories/fields.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :field do
     name { "#{Faker::Sports::Football.team} #{rand(100..999)}" }
-    price { Faker::Commerce.price(range: 50.0..200.0) }
+    price { Faker::Commerce.price(range: Settings.field.price_min..Settings.field.price_max) }
     grass { [:natural, :artificial].sample }
     capacity { [5, 7, 11].sample }
     open_time { Time.parse("08:00").strftime(Settings.calendar.time_format) }
