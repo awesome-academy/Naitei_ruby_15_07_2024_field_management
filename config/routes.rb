4b5595c40bf159fb03add2eba6b9216e3b19ecf8
duongdt0448 <do.thuy.duong@sun-asterisk.com>
2024-09-11 10:39:05 +0700

    google_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, only: :omniauth_callbacks,
+       controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   scope "(:locale)", locale: /en|vi/ do
     mount Sidekiq::Web => '/sidekiq'
-    devise_for :users, controllers: {
+    devise_for :users, skip: :omniauth_callbacks, controllers: {
       sessions: "users/sessions",
       registrations: "users/registrations",
       passwords: "users/passwords",
